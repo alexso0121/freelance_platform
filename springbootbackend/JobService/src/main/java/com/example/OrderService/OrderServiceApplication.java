bbbe320f335e6afb79ac4131640c6aa06a04c6df
alexso0121 <114412149+alexso0121@users.noreply.github.com>
2023-01-23 02:02:36 +0800

    orderjob

---
+++ springbootbackend/JobService/src/main/java/com/example/OrderService/OrderServiceApplication.java
@@ -0,0 +1,20 @@
+package com.example.OrderService;
+
+import com.example.OrderService.Entity.Location;
+import com.example.OrderService.Repository.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+
+
+
+}
